43dae9f8d89ad51084b28ce48cfeb73853b736cb
Salman Shaikh <sk.salman0425@gmail.com>
2023-02-12 19:11:01 +0530

    changes done in increment and decrement counter done

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
-
+//Here Count Componant is formed.
  const Count =(props)=>{
+  //in props object we get all attributes which are passed from parent to child through props. customcount,customhandleClick,customname.
   return(
-    <button onClick={()=>props.handleClick(props.name)}>{props.name}</button>
+    <button onClick={()=>props.customhandleClick(props.customname)}>{props.customname}</button>
   );
  };
 
@@ -14,9 +15,9 @@ function App() {
  
     // const [counter, setCounter]=useState(0);
   //   const handleOnIncrementClick=()=>{
-   //counter=counter+1 is wrong. we can can state value only through setter
+   //counter=counter+1 is wrong because we can set state value .i.e variable value only through setter function of useState().
   //   console.log("Increment counter click called");
-  //   setCounter(counter+1);
+  //   setCounter(counter+1);// this is rigth way of setting value of state in React.
   // };
   //   const handleOnDecrementClick =()=>{
   //     console.log("Decrement counter click called");
@@ -31,8 +32,13 @@ function App() {
     // }
     // }
     const [count,setCount]=useState(0);
+    //we put count state here because app.js is parent componant of Count componant 
+    //and we want state of both button should be depend on each other. 
+    //if we put these useSate in Count componant then each componant have their own variable with lexical enviroment
     const handleClick =(value)=>{
-      console.log('The value from child component',value);
+      //here value get Increment/dercrement  from count componant's callback function
+      // i.e.customhandleClick(props.customname). here data passed from child to parent through callback function 
+      console.log('The value from child component to parent componant',value);
       if(value==='Increment'){
         setCount(count+1);
       }else{
@@ -47,15 +53,18 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
 
-          {/* <button onClick={()=>handleOnClick('Increment')}>Counter Increment</button>
-          <button onClick={()=>handleOnClick('Decrement')}>Counter Decrement</button> */}
+          
           {/* <button onClick={handleOnIncrementClick}> Counter Increment</button>
-          here call back function has reference that's why you don't directly pass arg from it,
-           you have to write in an arrow function when you are passing arg as shown above. 
           <button onClick={handleOnDecrementClick}> Counter Decrement </button> */}
           {/* <div> {counter} </div> */}
-          <Count count={count} handleClick={handleClick} name='Increment'/>
-          <Count count={count} handleClick={handleClick} name='Decrement'/>
+          
+          {/* <button onClick={()=>handleOnClick('Increment')}>Counter Increment</button>
+          <button onClick={()=>handleOnClick('Decrement')}>Counter Decrement</button> 
+          here call back function has reference that's why you don't directly pass arg from it,
+           you have to write in an arrow function when you are passing arg as shown above.*/}
+          {/* here Count componant called twice.here data is passed to props through custom attribute like customcount,customhandleClick,customname*/}
+          <Count customcount={count} customhandleClick={handleClick} customname='Increment'/>
+          <Count customcount={count} customhandleClick={handleClick} customname='Decrement'/>
           <div> Counter: {count}</div>
          
         </p>
